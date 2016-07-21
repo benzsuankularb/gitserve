5af4105474b25c6316ea6b26fbb4f862b264e423
benzsuankularb <benzsk130@gmail.com>
2016-07-21 12:22:16 +0700

    add file

---
+++ gitserve.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	var repoName string
+	var workDir string
+	flag.StringVar(&repoName, "repo", "", "repositary name")
+	flag.StringVar(&workDir, "dir", "", "work directory")
+	flag.Parse()
+	if repoName == "" || workDir == "" {
+		fmt.Println("Error: repo & dir arguments are required")
+		os.Exit(1)
+	}
+	gitRoot := os.Getenv("GITROOT")
+	if gitRoot == "" {
+		fmt.Println("Error: $GITROOT required")
+		os.Exit(1)
+	}
+	gitDir := gitRoot + "/" + repoName + ".git"
+	os.MkdirAll(gitDir, 0777)
+	os.MkdirAll(workDir, 0777)
+	args := []string{"init", "--bare", gitDir}
+	if err := exec.Command("git", args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	data := []byte("#!/bin/sh\ngit --work-tree=" + workDir + " --git-dir=" + gitDir + " checkout -f")
+	ioutil.WriteFile(gitDir+"/hooks/post-receive", data, 777)
+	fmt.Println("SUCCESSFULL")
+}
